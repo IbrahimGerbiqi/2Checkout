2fafb36fb099f87f523da50db3d879b9bdad5abd
Ibrahim Gerbiqi <34714009+IbrahimG99@users.noreply.github.com>
2021-01-04 10:44:47 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const bodyparser = require("body-parser");
+const path = require("path");
+const app = express();
+var request = require("request");
+
+var Publishable_Key =
+    "pk_test_51HyzFwBYJ6atGlFKHSXnvfHr9aiXcR8vpVt3G3avkhmHi46xBoLGWoxQaLhhHEf2VGbNOk5zU5GDM6jMRgHHRJUX00Sl8yrH9E";
+
+const stripe = require("stripe")(
+    "sk_test_51HyzFwBYJ6atGlFKsB9dKGrzhfhQLHuFlEgYn9adQveF0qt5FiFfyBQ0usRpbqblwQObcTacJxvl9dCU0prFv20L00STXu55Ov"
+);
+
+const port = process.env.PORT || 3000;
+
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+
+// View Engine Setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/", function (req, res) {
+    res.render("Home", {
+        key: Publishable_Key,
+    });
+});
+
+app.get("/success", function (req, res) {
+    console.log("erdh qetu");
+    res.render("success");
+});
+
+app.listen(port, function (error) {
+    if (error) throw error;
+    console.log("Server created Successfully");
+});
